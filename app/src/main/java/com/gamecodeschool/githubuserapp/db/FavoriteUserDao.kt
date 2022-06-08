78c7bbb25789404a3ee1ad9db012c4a0fb1b43ef
aryapraset <aryaprasetiyo12@gmail.com>
2022-06-08 11:46:55 +0800

    Menambahkan Dao dan database

--- app/src/main/java/com/gamecodeschool/githubuserapp/db/FavoriteUserDao.kt
+++ app/src/main/java/com/gamecodeschool/githubuserapp/db/FavoriteUserDao.kt
@@ -1,4 +1,20 @@
 package com.gamecodeschool.githubuserapp.db
 
-interface FavoriteUserDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface FavoriteUserDao{
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addToFavorite(favoriteUser: FavoriteUser)
+
+    @Query("SELECT * FROM favorite_user")
+    fun getFavoriteUser(): LiveData<List<FavoriteUser>>
+
+    //untuk check
+    @Query("SELECT count(*) FROM favorite_user WHERE favorite_user.id = :id")
+    suspend fun checkUser(id: Int): Int
+
+    @Query("DELETE FROM favorite_user WHERE favorite_user.id = :id")
+    fun removeFromFavorite(id: Int): Int
 }
